0c5a5159552a4a570c0605268dd86f53d2f1b565
Njiiri Yvonne <njerinjiiri@gmail.com>
2021-11-29 02:33:16 -0500

    defined view function

--- MyGallery/views.py
+++ MyGallery/views.py
@@ -12,3 +12,22 @@ def images(request):
 
     context = {'categories': categories, 'photos': photos, 'locations': locations}
     return render(request,'photos.html', context)
+
+def search_results(request):
+
+    if 'image' in request.GET and request.GET["image"]:
+        search_term = request.GET.get("image")
+        searched_articles = Image.search_by_name(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'search.html',{"message":message,"articles": searched_articles})
+
+    else:
+        message = "You havent searched for any term"
+        return render(request, 'search.html',{"message":message})
+
+
+
+
+
+    
