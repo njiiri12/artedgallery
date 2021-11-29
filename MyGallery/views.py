a1e1f54125cfd901f74c2224284cd78a059e6f21
Njiiri Yvonne <njerinjiiri@gmail.com>
2021-11-29 02:27:04 -0500

    added  the image view

--- MyGallery/views.py
+++ MyGallery/views.py
@@ -2,3 +2,13 @@ from django.shortcuts import render
 from .models import Category, Image, Location
 
 # Create your views here.
+def images(request):
+    location = request.GET.get('location')
+    print('location:', location)
+
+    categories = Category.objects.all()
+    locations = Location.objects.all()
+    photos = Image.objects.all()
+
+    context = {'categories': categories, 'photos': photos, 'locations': locations}
+    return render(request,'photos.html', context)
