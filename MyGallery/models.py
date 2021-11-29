9cefa09fa6917cf04fd0f49ba198086d2b33d9dd
Njiiri Yvonne <njerinjiiri@gmail.com>
2021-11-29 00:49:40 -0500

    added the image module and its functionalities

--- MyGallery/models.py
+++ MyGallery/models.py
@@ -15,7 +15,7 @@ class Category(models.Model):
         self.delete()
 
 class Location(models.Model):
-    name = models.CharField(max_length= 30)
+    name = models.CharField(max_length= 50)
 
     def __str__(self):
         return self.name
@@ -25,3 +25,42 @@ class Location(models.Model):
 
     def delete_location(self):
         self.delete()
+
+
+class Image(models.Model):
+    image = models.ImageField(upload_to = 'photos/', default = None)
+    name = models.CharField(max_length= 40)
+    description = models.TextField()
+    location = models.ForeignKey(
+        Location, on_delete=models.CASCADE, default=None)
+    category = models.ManyToManyField(Category)
+
+    def __str__(self):
+        return self.name
+
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        self.delete()
+
+    @classmethod
+    def get_image_by_id(cls, id):
+        image = cls.objects.filter(id_icontains = id)
+        return image
+
+    @classmethod
+    def get_image_by_category(cls, category):
+        image = cls.objects.filter(category_icontains=category)
+        return image
+
+    @classmethod
+    def get_image_by_location(cls, location):
+        image = cls.objects.filter(location_icontains=location)
+        return image
+
+    @classmethod
+    def search_by_name(cls, search_term):
+        images = cls.objects.filter(name_icontains= search_term)
+        return images
+
