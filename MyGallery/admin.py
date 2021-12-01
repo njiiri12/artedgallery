617786ec1f9f742a7c3aead9f65d19504519890e
Njiiri Yvonne <njerinjiiri@gmail.com>
2021-12-01 00:12:41 -0500

    redistered models

--- MyGallery/admin.py
+++ MyGallery/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Image, Location, Category
 
 # Register your models here.
+
+class ImageAdmin(admin.ModelAdmin):
+    filter_horizontal = ('category',)
+
+admin.site.register(Category)
+admin.site.register(Image, ImageAdmin)
+admin.site.register(Location)
